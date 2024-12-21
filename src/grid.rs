4f3e6989faea923d2cf00f2c79ee3087354d7fef
Joe Endacott <joseph.endacott@gmail.com>
2024-12-21 11:14:38 +0000

    Added the functionality to add, subtract, multiply and divide grid functions and functionality to multiply a grid function by a scalar. Added a new function which generates a constant (flat) grid function. I have not tested these changes yet.

--- src/grid.rs
+++ src/grid.rs
@@ -49,6 +49,8 @@ impl Grid {
 /// sampling point corresponds to an element in the vector function_values. The
 /// sampling points are contained in the Grid grid.
 ///
+/// Example use case: todo: add example use case.
+///
 #[derive(Debug)]
 pub struct GridFunction {
     pub grid: Grid,
@@ -61,6 +63,8 @@ impl GridFunction {
     /// Description: this function generates a grid function, given a
     /// real-valued function of a real variable func and a Grid grid.
     ///
+    /// Example use case: todo: add example use case.
+    ///
     pub fn new_grid_function<F>(grid: Grid, func: F) -> Self
     where
         F: Fn(f64) -> f64,
@@ -68,6 +72,224 @@ impl GridFunction {
         // Creates a vector containing the value of func at each grid point.
         let function_values: Vec<f64> =
             grid.grid_points.iter().map(|&x| func(x)).collect();
+
+        GridFunction {
+            grid,
+            function_values,
+        }
+    }
+
+    /// # New constant grid function
+    ///
+    /// Description: this function generates a constant (flat) grid function
+    /// with height scalar, given a Grid grid.
+    ///
+    /// Example use case: todo: add example use case.
+    ///
+    pub fn new_constant_grid_function(grid: Grid, scalar: f64) -> Self {
+        let function_values: Vec<f64> =
+            grid.grid_points.iter().map(|_| scalar).collect();
+
+        GridFunction {
+            grid,
+            function_values,
+        }
+    }
+
+    /// # Grid function addition
+    ///
+    /// Description: this function sums grid_function_1 and grid_function_2,
+    /// and returns the result.
+    /// The grid functions should both have the same grid. If they have
+    /// different grids, the grid from grid_function_1 will be used.
+    ///
+    /// Example use case: todo: add example use case.
+    ///
+    pub fn grid_function_add(
+        grid_function_1: &GridFunction,
+        grid_function_2: &GridFunction,
+    ) -> Self {
+        let grid = grid_function_1.grid.clone();
+
+        let function_values_1 = grid_function_1.function_values.clone();
+        let mut function_values_2 = grid_function_2.function_values.clone();
+
+        let length_difference =
+            function_values_1.len() - function_values_2.len();
+
+        // If function_values_2 has fewer elements than function_values_1, adds
+        // zeroes to the end of function_values_2 until the two vectors are the
+        // same length.
+        if length_difference > 0 {
+            for _ in 0..length_difference {
+                function_values_2.push(0.0);
+            }
+        }
+
+        // Iterates over all the elements in function_values_1 and adds them to
+        // the elements in function_values_2.
+        let function_values: Vec<f64> = function_values_1
+            .iter()
+            .zip(function_values_2.iter())
+            .map(|(x, y)| x + y)
+            .collect();
+
+        GridFunction {
+            grid,
+            function_values,
+        }
+    }
+
+    /// # Grid function subtraction
+    ///
+    /// Description: this function subtracts grid_function_2 from
+    /// grid_function_1, and returns the result.
+    /// The grid functions should both have the same grid. If they have
+    /// different grids, the grid from grid_function_1 will be used.
+    ///
+    /// Example use case: todo: add example use case.
+    ///
+    pub fn grid_function_subtract(
+        grid_function_1: &GridFunction,
+        grid_function_2: &GridFunction,
+    ) -> Self {
+        let grid = grid_function_1.grid.clone();
+
+        let function_values_1 = grid_function_1.function_values.clone();
+        let mut function_values_2 = grid_function_2.function_values.clone();
+
+        let length_difference =
+            function_values_1.len() - function_values_2.len();
+
+        // If function_values_2 has fewer elements than function_values_1, adds
+        // zeroes to the end of function_values_2 until the two vectors are the
+        // same length.
+        if length_difference > 0 {
+            for _ in 0..length_difference {
+                function_values_2.push(0.0);
+            }
+        }
+
+        // Iterates over all the elements in function_values_1 and adds them to
+        // the elements in function_values_2.
+        let function_values: Vec<f64> = function_values_1
+            .iter()
+            .zip(function_values_2.iter())
+            .map(|(x, y)| x - y)
+            .collect();
+
+        GridFunction {
+            grid,
+            function_values,
+        }
+    }
+
+    /// # Grid function multiplication
+    ///
+    /// Description: this function takes the product of grid_function_1 and
+    /// grid_function_2, and returns the result.
+    /// The grid functions should both have the same grid. If they have
+    /// different grids, the grid from grid_function_1 will be used.
+    ///
+    /// Example use case: todo: add example use case.
+    ///
+    pub fn grid_function_multiply(
+        grid_function_1: &GridFunction,
+        grid_function_2: &GridFunction,
+    ) -> Self {
+        let grid = grid_function_1.grid.clone();
+
+        let function_values_1 = grid_function_1.function_values.clone();
+        let mut function_values_2 = grid_function_2.function_values.clone();
+
+        let length_difference =
+            function_values_1.len() - function_values_2.len();
+
+        // If function_values_2 has fewer elements than function_values_1, adds
+        // zeroes to the end of function_values_2 until the two vectors are the
+        // same length.
+        if length_difference > 0 {
+            for _ in 0..length_difference {
+                function_values_2.push(0.0);
+            }
+        }
+
+        // Iterates over all the elements in function_values_1 and adds them to
+        // the elements in function_values_2.
+        let function_values: Vec<f64> = function_values_1
+            .iter()
+            .zip(function_values_2.iter())
+            .map(|(x, y)| x * y)
+            .collect();
+
+        GridFunction {
+            grid,
+            function_values,
+        }
+    }
+
+    /// # Grid function division
+    ///
+    /// Description: this function divides grid_function_1 by grid_function_2,
+    /// and returns the result.
+    /// The grid functions should both have the same grid. If they have
+    /// different grids, the grid from grid_function_1 will be used.
+    ///
+    /// Example use case: todo: add example use case.
+    ///
+    pub fn grid_function_division(
+        grid_function_1: &GridFunction,
+        grid_function_2: &GridFunction,
+    ) -> Self {
+        let grid = grid_function_1.grid.clone();
+
+        let function_values_1 = grid_function_1.function_values.clone();
+        let mut function_values_2 = grid_function_2.function_values.clone();
+
+        let length_difference =
+            function_values_1.len() - function_values_2.len();
+
+        // If function_values_2 has fewer elements than function_values_1, adds
+        // zeroes to the end of function_values_2 until the two vectors are the
+        // same length.
+        if length_difference > 0 {
+            for _ in 0..length_difference {
+                function_values_2.push(0.0);
+            }
+        }
+
+        // Iterates over all the elements in function_values_1 and adds them to
+        // the elements in function_values_2.
+        let function_values: Vec<f64> = function_values_1
+            .iter()
+            .zip(function_values_2.iter())
+            .map(|(x, y)| x / y)
+            .collect();
+
+        GridFunction {
+            grid,
+            function_values,
+        }
+    }
+
+    /// # Grid function scalar multiplication
+    ///
+    /// Description: this function multiples grid_function by a scalar,
+    /// and returns the result.
+    ///
+    /// Example use case: todo: add example use case.
+    ///
+    pub fn grid_function_scalar_multiplication(
+        grid_function: &GridFunction,
+        scalar: f64,
+    ) -> Self {
+        let grid = grid_function.grid.clone();
+        let function_values = grid_function.function_values.clone();
+
+        // Iterates over all the elements in function_values multiplies each
+        // value by scalar
+        let function_values: Vec<f64> =
+            function_values.iter().map(|x| scalar * x).collect();
 
         GridFunction {
             grid,
